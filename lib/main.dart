bd072a31e07d1cfc9bb795cca83eb1a1e978486d
Salma666E <smsmacs666@gmail.com>
2022-10-03 12:16:24 +0200

    build theme layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:todo/ui/pages/notification_screen.dart';
+import 'package:todo/ui/theme.dart';
 
 import 'ui/pages/home_page.dart';
 
@@ -16,10 +17,9 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'todo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.teal,
-        backgroundColor: Colors.teal,
-      ),
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeMode.dark,
       home: const NotificationScreen(payload: 'ssssssssss|xss|10/10'),
     );
   }
