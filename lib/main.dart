d7fc9366e39faa40daa792d9fb1719083d4fbe6a
Salma666E <smsmacs666@gmail.com>
2023-01-19 09:06:56 +0200

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:todo/ui/pages/notification_screen.dart';
 import 'package:todo/ui/theme.dart';
 
+import 'services/theme_services.dart';
 import 'ui/pages/home_page.dart';
 
 void main() {
@@ -19,8 +20,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: Themes.light,
       darkTheme: Themes.dark,
-      themeMode: ThemeMode.dark,
-      home: const NotificationScreen(payload: 'ssssssssss|xss|10/10'),
+      themeMode: ThemeServices().theme,
+      home: const HomePage(),
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/services/theme_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.brightness_high_sharp),
+          onPressed: () {
+            ThemeServices().switchTheme();
+          },
+        ),
+      ),
       body: Container(),
     );
   }
